e2bbc87e43ff568b391a1da6b488692e2e0d8033
Hugo Locurcio <hugo.l@openmailbox.org>
2016-11-02 09:53:36 +0100

    More GUI elements themed
    - Label
    - TextEdit
    - HScrollBar/VScrollBar
    - HSlider
    - SpinBox
    - WindowDialog
    - and more...

--- example.gd
+++ example.gd
@@ -1,6 +1,7 @@
 extends Control
 
 onready var control_gallery = get_node("Panel/ControlGallery")
+onready var window_dialog = get_node("WindowDialog")
 
 func _ready():
 	control_gallery.hide()
@@ -10,3 +11,6 @@ func _on_ClickMeForControlGallery_pressed():
 	
 func _on_QuitDemo_pressed():
 	get_tree().quit()
+
+func _on_ShowWindowDialog_pressed():
+	window_dialog.popup()
